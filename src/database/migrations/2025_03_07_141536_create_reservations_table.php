27fee15750faeb75ea4edb54ebad6988f12343f3
Seiya <stachizaki71@gmail.com>
2025-03-07 23:45:56 +0900

    テーブルとモデルの作成

---
+++ src/database/migrations/2025_03_07_141536_create_reservations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('reservation_datetime');
+            $table->integer('guest_count');
+            $table->foreignId('shop_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
