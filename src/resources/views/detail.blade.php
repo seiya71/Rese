15534135c3a24e3361f37def7e720fad756cfd75
Seiya <stachizaki71@gmail.com>
2025-03-21 15:54:15 +0900

    スタイルの作成

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -7,45 +7,68 @@
 @section('content')
     <div class="shop-details">
         <div class="detail-left">
-            <h2>{{ $shop->shop_name }}</h2>
-            <img src="{{ asset('images/' . $shop->image) }}" alt="{{ $shop->shop_name }}">
-            <p>#{{ $shop->area->area_name }}</p>
-            <p>#{{ $shop->genre->genre_name }}</p>
-            <p>{{ $shop->introduction }}</p>
+            <h2 class="shop-name">{{ $shop->shop_name }}</h2>
+            <img class="shop-image" src="{{ asset('images/' . $shop->image) }}" alt="{{ $shop->shop_name }}">
+            <div class="category">
+                <p class="category-item">#{{ $shop->area->area_name }}</p>
+                <p class="category-item">#{{ $shop->genre->genre_name }}</p>
+            </div>
+            <p class="introduction">{{ $shop->introduction }}</p>
         </div>
         <div class="detail-right">
-            <form action="{{ route('detail', $shop->id) }}" method="GET">
-                <input type="date" name="date" value="{{ session('temp_reservation.date') }}" onchange="this.form.submit()"
-                    required>
+            <div class="reserve">
+                <h3 class="reserve-title">予約</h3>
+                <form class="reserve-input" action="{{ route('detail', $shop->id) }}" method="GET">
+                    <input class="reserve-input__item" type="date" name="date" value="{{ session('temp_reservation.date') }}" onchange="this.form.submit()"
+                        required>
 
-                <select name="time" onchange="this.form.submit()" required>
-                    @for ($hour = 10; $hour < 24; $hour++)
-                        @php $time = sprintf('%02d:00', $hour); @endphp
-                        <option value="{{ $time }}" {{ session('temp_reservation.time') == $time ? 'selected' : '' }}>
-                            {{ $time }}
-                        </option>
-                    @endfor
-                </select>
+                    <select class="reserve-input__item" name="time" onchange="this.form.submit()" required>
+                        @for ($hour = 10; $hour < 24; $hour++)
+                            @php $time = sprintf('%02d:00', $hour); @endphp
+                            <option value="{{ $time }}" {{ session('temp_reservation.time') == $time ? 'selected' : '' }}>
+                                {{ $time }}
+                            </option>
+                        @endfor
+                    </select>
 
-                <select name="guest_count" onchange="this.form.submit()" required>
-                    @for ($i = 1; $i <= 10; $i++)
-                        <option value="{{ $i }}" {{ session('temp_reservation.guest_count') == $i ? 'selected' : '' }}>
-                            {{ $i }}人
-                        </option>
-                    @endfor
-                </select>
-            </form>
-            <div class="reservation-preview">
-                <h3>予約内容の確認</h3>
-                <p>日付: {{ session('temp_reservation.date', '選択してください') }}</p>
-                <p>時間: {{ session('temp_reservation.time', '選択してください') }}</p>
-                <p>人数: {{ session('temp_reservation.guest_count', '選択してください') }}人</p>
+                    <select class="reserve-input__item" name="guest_count" onchange="this.form.submit()" required>
+                        @for ($i = 1; $i <= 10; $i++)
+                            <option value="{{ $i }}" {{ session('temp_reservation.guest_count') == $i ? 'selected' : '' }}>
+                                {{ $i }}人
+                            </option>
+                        @endfor
+                    </select>
+                </form>
+                <div class="reservation-info">
+                    <div class="info-row">
+                        <div class="label">Shop</div>
+                        <div class="value">{{ $shop->shop_name }}</div>
+                    </div>
+                    <div class="info-row">
+                        <div class="label">Date</div>
+                        <div class="value">
+                            {{ session('temp_reservation.date', '選択してください') }}
+                        </div>
+                    </div>
+                    <div class="info-row">
+                        <div class="label">Time</div>
+                        <div class="value">
+                            {{ session('temp_reservation.time', '選択してください') }}
+                        </div>
+                    </div>
+                    <div class="info-row">
+                        <div class="label">Number</div>
+                        <div class="value">
+                            {{ session('temp_reservation.guest_count', '選択してください') }}人
+                        </div>
+                    </div>
+                </div>
+
+                <form class="reserve-submit-form" action="{{ route('reservation', $shop->id) }}" method="POST">
+                    @csrf
+                    <button class="reserve-button" type="submit">予約する</button>
+                </form>
             </div>
-
-            <form action="{{ route('reservation', $shop->id) }}" method="POST">
-                @csrf
-                <button type="submit">予約する</button>
-            </form>
         </div>
     </div>
 @endsection
--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -5,63 +5,78 @@
 @endsection
 
 @section('content')
-    <h2>{{ $user->name }}</h2>
-    <div>
-        <h3>予約状況</h3>
-        <div>
-        @foreach ($reservations as $reservation)
-            <div>
-                <div>
-                    <p>予約{{ $loop->iteration }}</p>
-                    <form action="{{ route('reservation.cancel', $reservation->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit">
-                            ✕
-                        </button>
-                    </form>
+    <h2 class="user-name">{{ $user->name }}さん</h2>
+    <div class="user-info">
+        <div class="info-left">
+            <h3 class="info-title">予約状況</h3>
+            @foreach ($reservations as $reservation)
+                <div class="reserve-info">
+                    <div class="reserve-nav">
+                        <p class="reserve-title">予約{{ $loop->iteration }}</p>
+                        <form class="reserve-cancel" action="{{ route('reservation.cancel', $reservation->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="cancel-button" type="submit">
+                                ✕
+                            </button>
+                        </form>
+                    </div>
+                    <div class="info-detail">
+                        <div class="info-row">
+                            <div class="label">Shop</div>
+                            <div class="value">{{ $reservation->shop->shop_name }}</div>
+                        </div>
+                        <div class="info-row">
+                            <div class="label">Date</div>
+                            <div class="value">
+                                {{ Carbon\Carbon::parse($reservation->reservation_datetime)->toDateString() }}
+                            </div>
+                        </div>
+                        <div class="info-row">
+                            <div class="label">Time</div>
+                            <div class="value">
+                                {{ Carbon\Carbon::parse($reservation->reservation_datetime)->format('H:i') }}
+                            </div>
+                        </div>
+                        <div class="info-row">
+                            <div class="label">Number</div>
+                            <div class="value">
+                                {{ $reservation->guest_count }}人
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <span>Shop</span>
-                    <span>{{ $reservation->shop->shop_name }}</span>
-                </div>
-                <div>
-                    <span>Date</span>
-                    <span>
-                        {{ Carbon\Carbon::parse($reservation->reservation_datetime)->toDateString() }}
-                    </span>
-                </div>
-                <div>
-                    <span>Time</span>
-                    <span>
-                        {{ Carbon\Carbon::parse($reservation->reservation_datetime)->format('H:i') }}
-                    </span>
-                </div>
-                <div>
-                    <span>Number</span>
-                    <span>{{ $reservation->guest_count }}人</span>
-                </div>
+            @endforeach
+        </div>
+        <div class="info-right">
+            <h3 class="info-title">お気に入り店舗</h3>
+            <div class="shop-list">
+                @foreach ($likeShops as $like)
+                    <div class="shop-card">
+                        <img class="shop-image" src="" alt="店舗画像">
+                        <div class="shop-info">
+                            <h3 class="shop-name">{{ $like->shop->shop_name }}</h3>
+                            <div class="shop-meta">
+                                <p class="shop-category">
+                                    <span>#</span> {{ $like->shop->area->area_name }}
+                                </p>
+                                <p class="shop-category">
+                                    <span>#</span> {{ $like->shop->genre->genre_name }}
+                                </p>
+                            </div>
+                            <div class="shop-actions">
+                                <a class="detail-button" href="{{ url('/detail', $like->shop->id) }}">詳細を見る</a>
+                                <form class="favorite-form" action="{{ route('togglelike', $like->shop->id) }}" method="POST">
+                                    @csrf
+                                    <button class="favorite-button" type="submit">
+                                        <img class="favorite-heart" src="{{ asset('images/red-heart.svg') }}" alt="お気に入り解除" width="24" height="24">
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
-    </div>
-    <div>
-        <h3>お気に入り店舗</h3>
-        @foreach ($likeShops as $like)
-            <div>
-                <img src="" alt="店舗画像">
-                <h3>{{ $like->shop->shop_name }}</h3>
-                <span>#</span>
-                <span>{{ $like->shop->area->area_name }}</span>
-                <span>#</span>
-                <span>{{ $like->shop->genre->genre_name }}</span>
-                <form action="{{ route('togglelike', $like->shop->id) }}" method="POST">
-                    @csrf
-                    <button type="submit" class="status-img">
-                        <img src="{{ asset('images/red-heart.svg') }}" alt="お気に入り解除" width="24" height="24">
-                    </button>
-                </form>
-                <a href="{{ url('/detail', $like->shop->id) }}" class="btn btn-outline-primary">詳細を見る</a>
-            </div>
-        @endforeach
+        </div>
     </div>
 @endsection
